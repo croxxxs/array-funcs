df34e18d93ea27e2f81e822a466da866b7d613b9
croxxxs <konchihirose@gmail.com>
2025-03-06 18:12:47 +0300

    added third func

--- task.py
+++ task.py
@@ -16,4 +16,11 @@ def filter_greater(arr,value):
     print(new_arr)
     return new_arr
 
-
+def filter_equal(arr,value):
+    new_arr = []
+    value = int(input('enter value > '))
+    for i in arr:
+        if i == value:
+            new_arr.append(i)
+    print(new_arr)
+    return new_arr
