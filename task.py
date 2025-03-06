60766ab261b7fe62177127f36a9bea9b42737dd6
croxxxs <konchihirose@gmail.com>
2025-03-06 18:24:53 +0300

    added new func
    added increadeable func thats checks how are working other funcs

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 value = int(input('enter value > '))
 
+arr = [1,2,3,4,7,12,32,42]
+
 def filter_less(arr,value):
     new_arr = []
     
@@ -35,3 +37,9 @@ def filter_not_equal(arr,value):
             new_arr.append(i)
     print(new_arr)
     return new_arr
+
+def test_funcs():
+    print(f'filter equal results: {filter_equal(arr,value)}')
+    print(f'filter not equal results: {filter_not_equal(arr,value)}')
+    print(f'filter greater results: {filter_greater(arr,value)}')
+    print(f'filter greater results: {filter_less(arr,value)}')
