3b9b8f70d94927d091645967b2d441b1f4bafe6c
croxxxs <konchihirose@gmail.com>
2025-03-06 18:13:38 +0300

    added fourth func

--- task.py
+++ task.py
@@ -24,3 +24,12 @@ def filter_equal(arr,value):
             new_arr.append(i)
     print(new_arr)
     return new_arr
+
+def filter_not_equal(arr,value):
+    new_arr = []
+    value = int(input('enter value > '))
+    for i in arr:
+        if i != value:
+            new_arr.append(i)
+    print(new_arr)
+    return new_arr
