d7b43a9b41c202099bd0a822880e68a141e3c417
croxxxs <konchihirose@gmail.com>
2025-03-06 18:12:12 +0300

    added second func

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
-def filter_greater(arr,value):
+def filter_less(arr,value):
     new_arr = []
     value = int(input('enter value > '))
     for i in arr:
@@ -6,4 +6,14 @@ def filter_greater(arr,value):
             new_arr.append(i)
     print(new_arr)
     return new_arr
+
+def filter_greater(arr,value):
+    new_arr = []
+    value = int(input('enter value > '))
+    for i in arr:
+        if i > value:
+            new_arr.append(i)
+    print(new_arr)
+    return new_arr
+
 
